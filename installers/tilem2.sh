3adc83fb147de217560de56fe10dbbb7f710ac8f
amihart <ameliahartman.s@gmail.com>
2014-01-15 16:22:31 -0500

    tilem

---
+++ installers/tilem2.sh
@@ -0,0 +1,7 @@
+echo Downloading...
+cd ~/Downloads
+wget raw.github.com/amihart/charm/master/files/tilem2
+echo Installing...
+sudo chmod +x tilem2
+sudo mv tilem2 /usr/local/bin/tilem
+echo Installed successfully. 

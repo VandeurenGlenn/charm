a19bb364c2c9e75d6567e56b95f049b3e3fabf92
amihart <ameliahartman.s@gmail.com>
2014-01-16 14:11:58 -0500

    xdotool

---
+++ installers/xdotool.sh
@@ -0,0 +1,16 @@
+echo Downloading package...
+cd ~/Downloads
+wget raw.github.com/amihart/charm/master/files/xdotool.tar.gz
+echo Extracting package...
+sudo tar -zxvf xdotool.tar.gz
+echo Installing...
+if [ ! -d "/usr/local/lib" ]; then
+   sudo mkdir "/usr/local/lib"
+fi
+sudo chmod +x xdotool
+sudo mv xdotool /usr/local/bin
+sudo mv libxdo.so.3 /usr/local/lib
+
+echo Cleaning up...
+sudo rm xdotool.tar.gz
+echo Installed successfully. 
